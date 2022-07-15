3feae408e72fb134b12798dbebdff54cf8b0bbda
yellow_may <yellow.may.21@gmail.com>
2022-07-15 19:43:40 +0100

    Added React Query request

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,18 +1,25 @@
-import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import { BrowserRouter } from 'react-router-dom';
+import { ChakraProvider, ColorModeScript } from '@chakra-ui/react';
 import theme from 'theme';
 import App from 'App';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-	<React.StrictMode>
-		<BrowserRouter>
-			<ColorModeScript initialColorMode={theme.config.intialColorMode} />
+const client = new QueryClient();
+createRoot(document.getElementById('root') as HTMLElement).render(
+	<StrictMode>
+		<QueryClientProvider client={client}>
+			<BrowserRouter>
+				<ColorModeScript initialColorMode={theme.config.intialColorMode} />
 
-			<ChakraProvider theme={theme}>
-				<App />
-			</ChakraProvider>
-		</BrowserRouter>
-	</React.StrictMode>
+				<ChakraProvider theme={theme}>
+					<App />
+				</ChakraProvider>
+			</BrowserRouter>
+
+			<ReactQueryDevtools initialIsOpen={false} position='bottom-right' />
+		</QueryClientProvider>
+	</StrictMode>
 );
--- client/src/pages/Countries.tsx
+++ client/src/pages/Countries.tsx
@@ -1,8 +1,41 @@
-import { Box, Grid, GridItem, VStack } from '@chakra-ui/react';
+import { Fragment, useEffect } from 'react';
+import axios from 'axios';
+import { useInView } from 'react-intersection-observer';
+import { useInfiniteQuery } from 'react-query';
+import { Box, Grid, GridItem, Spinner, VStack } from '@chakra-ui/react';
 import CountryCard from 'components/CountryCard';
 import SearchAndFilter from 'components/SearchAndFilter';
 
+interface CountryProps {
+	flag: string;
+	name: string;
+	population: number;
+	region: string;
+	capital: string;
+}
+
 const Countries = () => {
+	const { ref, inView } = useInView();
+
+	const { data, hasNextPage, fetchNextPage, isFetchingNextPage } =
+		useInfiniteQuery(
+			'countries',
+			async ({ pageParam = 0 }) => {
+				const res = await axios.get(
+					'http://localhost:5000/countries?currPage=' + pageParam
+				);
+				return res.data;
+			},
+			{ getNextPageParam: lastPage => lastPage.nextId ?? undefined }
+		);
+
+	useEffect(() => {
+		if (inView) {
+			fetchNextPage();
+		}
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [inView]);
+
 	return (
 		<VStack align='stretch' spacing={14}>
 			<SearchAndFilter />
@@ -16,9 +49,19 @@ const Countries = () => {
 					]}
 					gap={16}>
 					<GridItem>
-						<CountryCard />
+						{data &&
+							data.pages.map(page => (
+								<Fragment key={page.nextId}>
+									{page.countries.map((country: CountryProps) => (
+										<CountryCard {...country} />
+									))}
+								</Fragment>
+							))}
 					</GridItem>
 				</Grid>
+			</Box>
+			<Box ref={ref}>
+				<Spinner display={!hasNextPage || isFetchingNextPage ? 'none' : ''} />
 			</Box>
 		</VStack>
 	);
