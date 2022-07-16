2e46afa67e252c1e6e5872588c728d6bea180672
yellow_may <yellow.may.21@gmail.com>
2022-07-16 10:41:20 +0100

    Added Cors

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const path = require('node:path');
 const root = require('app-root-path');
+const cors = require('cors');
 const apiRouter = require('./apis');
 
 const app = express();
 
 // custom api router
+app.use(cors());
 app.use('/api', apiRouter);
 
 // serve static files
