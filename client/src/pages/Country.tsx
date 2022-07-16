c39d7adeb5219492d4ab7e99050517657b83ca3d
yellow_may <yellow.may.21@gmail.com>
2022-07-16 11:07:46 +0100

    Country Page done

--- client/src/pages/Country.tsx
+++ client/src/pages/Country.tsx
@@ -3,18 +3,33 @@ import {
 	AspectRatio,
 	Box,
 	Button,
+	Center,
 	Flex,
 	Grid,
 	GridItem,
 	Heading,
 	Image,
+	Spinner,
 	VStack,
+	Text,
 } from '@chakra-ui/react';
-import { ArrowBackIcon } from '@chakra-ui/icons';
+import { ArrowBackIcon, RepeatIcon } from '@chakra-ui/icons';
+import { useQuery } from 'react-query';
 import Feature from 'components/Feature';
+import { useLocation } from 'react-router';
+import axios from 'axios';
 
 const Country = () => {
 	const navigate = useNavigate();
+	const { pathname: name } = useLocation();
+
+	const { isLoading, isError, data, refetch } = useQuery(
+		['country', name],
+		async () => {
+			const res = await axios.get(`http://localhost:5000/api/country${name}`);
+			return res.data;
+		}
+	);
 
 	return (
 		<Box px={[3, 0]}>
@@ -23,66 +38,100 @@ const Country = () => {
 					Back
 				</Button>
 
-				<Flex w='100%' direction={['column', 'row']} gap={12}>
-					<Box w='100%'>
-						<AspectRatio ratio={4 / 3}>
-							<Image
-								src='https://bit.ly/naruto-sage'
-								alt='naruto'
-								objectFit='cover'
-							/>
-						</AspectRatio>
-					</Box>
+				{isLoading && (
+					<Center h={200} w='100%'>
+						<Spinner />
+					</Center>
+				)}
 
-					<Box w='100%'>
-						<VStack
-							minW={['30%', '30%', '30%', '50%']}
-							align='stretch'
-							spacing={[6, 8, 10]}>
-							<Heading as='h2' size='md'>
-								Konoha
-							</Heading>
+				{isError && (
+					<Flex
+						direction='column'
+						align='center'
+						justify='center'
+						gap={8}
+						h={250}
+						w='100%'>
+						<Text>There was an error, please try again</Text>
 
-							<Grid
-								templateColumns={[
-									'repeat(1, 1fr)',
-									'repeat(1, 1fr)',
-									'repeat(1, 1fr)',
-									'repeat(2, 1fr)',
-								]}
-								gap={[8, 12, 12, 1]}>
-								<GridItem>
-									<VStack align='stretch'>
-										<Feature title='Native Name' desc='Belgie' />
-										<Feature title='Population' desc='10,000,000' />
-										<Feature title='Region' desc='Europe' />
-										<Feature title='Sub Region' desc='Western Europe' />
-										<Feature title='Capital' desc='Brussels' />
-									</VStack>
-								</GridItem>
+						<Button onClick={() => refetch()}>
+							<RepeatIcon />
+						</Button>
+					</Flex>
+				)}
 
-								<GridItem>
-									<VStack align='stretch'>
-										<Feature title='Top Level Domain' desc='.be' />
-										<Feature title='Currencies' desc={['Euro']} />
-										<Feature
-											title='Languages'
-											desc={['Dutch', 'French', 'German']}
-										/>
-									</VStack>
-								</GridItem>
-							</Grid>
-						</VStack>
-
-						<Box mt={[12, 12, 14, 20]}>
-							<Feature
-								title='Border Countries'
-								desc={['France', 'Germany', 'Netherlands']}
-								boxed
-							/>
+				{data && (
+					<Flex w='100%' direction={['column', 'row']} gap={12}>
+						<Box w='100%'>
+							<AspectRatio ratio={4 / 3}>
+								<Image src={data.flag} alt='naruto' objectFit='cover' />
+							</AspectRatio>
 						</Box>
-					</Box>
-				</Flex>
+
+						<Box w='100%'>
+							<VStack
+								minW={['30%', '30%', '30%', '50%']}
+								align='stretch'
+								spacing={[6, 8, 10]}>
+								<Heading as='h2' size='md'>
+									{data.name}
+								</Heading>
+
+								<Grid
+									templateColumns={[
+										'repeat(1, 1fr)',
+										'repeat(1, 1fr)',
+										'repeat(1, 1fr)',
+										'repeat(2, 1fr)',
+									]}
+									gap={[8, 12, 12, 1]}>
+									<GridItem>
+										<VStack align='stretch'>
+											{data.nativeName && (
+												<Feature title='Native Name' desc={data.nativeName} />
+											)}
+											{data.population && (
+												<Feature title='Population' desc={data.population} />
+											)}
+											{data.region && (
+												<Feature title='Region' desc={data.region} />
+											)}
+											{data.subregion && (
+												<Feature title='Sub Region' desc={data.subregion} />
+											)}
+											{data.capital && (
+												<Feature title='Capital' desc={data.capital} />
+											)}
+										</VStack>
+									</GridItem>
+
+									<GridItem>
+										<VStack align='stretch'>
+											{data.tld && (
+												<Feature title='Top Level Domain' desc={data.tld} />
+											)}
+											{data?.currencies && (
+												<Feature title='Currencies' desc={data.currencies} />
+											)}
+											{data?.languages && (
+												<Feature title='Languages' desc={data.languages} />
+											)}
+											{data?.timezones && (
+												<Feature title='Timezones' desc={data.timezones} />
+											)}
+										</VStack>
+									</GridItem>
+								</Grid>
+							</VStack>
+
+							{data?.borders && (
+								<Box mt={[12, 12, 14, 20]}>
+									<Feature title='Border Countries' desc={data.borders} boxed />
+								</Box>
+							)}
+						</Box>
+					</Flex>
+				)}
 			</VStack>
 		</Box>
 	);
